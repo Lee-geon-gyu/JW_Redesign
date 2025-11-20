46e0ce7e05d3892a5ec024d33333da048d2a874d
GYUS <gungyu0230@gmail.com>
2025-11-20 12:33:43 +0900

    section-2 CLEAR

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
 // menuboxDropdown ------------------------------ //
 function menuboxDropdown__init() {
   $("header > .hd-container > .menu-box").mouseenter(function () {
@@ -7,7 +10,7 @@ function menuboxDropdown__init() {
   $("header > .hd-container > .menu-box").mouseleave(function () {
     $(this).find("> ul > .bg-container").stop(true, true).slideUp(300);
   });
-};
+}
 // menuitemDropdown ------------------------------ //
 function menuitemDropdown__init() {
   $("header > .hd-container > .menu-box").mouseenter(function () {
@@ -22,118 +25,144 @@ function menuitemDropdown__init() {
       self.find(".menu-item-container > ul").removeClass("fade");
     }, 150);
   });
-};
+}
 // translateboxDropdown ------------------------------ //
 function translateboxDropdown__init() {
-  $("header > .hd-container > .side-box > .language-box > .translate-box-1").click(function() {
+  $("header > .hd-container > .side-box > .language-box > .translate-box-1").click(function () {
     $(this).parent().find("> .translate-box-2").toggleClass("drop");
     $(this).find("> a > img").toggleClass("rotate");
   });
 
-  $("header > .hd-container > .side-box > .language-box > .translate-box-2 > a > .languages-wrapper > span").mouseenter(function() {
+  $("header > .hd-container > .side-box > .language-box > .translate-box-2 > a > .languages-wrapper > span").mouseenter(function () {
     $(this).addClass("hover");
   });
 
-  $("header > .hd-container > .side-box > .language-box > .translate-box-2 > a > .languages-wrapper > span").mouseleave(function() {
+  $("header > .hd-container > .side-box > .language-box > .translate-box-2 > a > .languages-wrapper > span").mouseleave(function () {
     $(this).removeClass("hover");
   });
 
-  $("header > .hd-container > .side-box > .language-box > .translate-box-2").click(function() {
-    $(this).removeClass("drop")
+  $("header > .hd-container > .side-box > .language-box > .translate-box-2").click(function () {
+    $(this).removeClass("drop");
     $(this).parent().find("> .translate-box-1 > a > img").toggleClass("rotate");
   });
 
-  $(document).click(function(e) {
-  const $dropdown = $("header > .hd-container > .side-box > .language-box > .translate-box-2");
-  const $button = $("header > .hd-container > .side-box > .language-box > .translate-box-1");
+  $(document).click(function (e) {
+    const $dropdown = $("header > .hd-container > .side-box > .language-box > .translate-box-2");
+    const $button = $("header > .hd-container > .side-box > .language-box > .translate-box-1");
 
-  if (!$button.is(e.target) && $button.has(e.target).length === 0 &&
-      !$dropdown.is(e.target) && $dropdown.has(e.target).length === 0) {
-    $dropdown.removeClass("drop");
-    $button.find("> a > img").removeClass("rotate");
-  }
-});
-};
+    if (!$button.is(e.target) && $button.has(e.target).length === 0 && !$dropdown.is(e.target) && $dropdown.has(e.target).length === 0) {
+      $dropdown.removeClass("drop");
+      $button.find("> a > img").removeClass("rotate");
+    }
+  });
+}
 // swiperCustom ------------------------------ //
 function swiperCustom__init() {
-const mySwiper = new Swiper(".mySwiper", {
-  loop: true,
-  speed: 600,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false
+  const mySwiper = new Swiper(".mySwiper", {
+    loop: true,
+    speed: 600,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+  });
+
+  const total = document.querySelector(".custom-pagination .total");
+  const current = document.querySelector(".custom-pagination .current");
+  const progress = document.querySelector(".custom-pagination .progress");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+
+  const totalSlides = mySwiper.slides.length;
+  total.textContent = String(totalSlides).padStart(2, "0");
+
+  function updatePagination() {
+    const index = mySwiper.realIndex + 1;
+
+    current.textContent = String(index).padStart(2, "0");
+
+    const percent = (index / totalSlides) * 100;
+    progress.style.width = percent + "%";
   }
-});
 
-const total = document.querySelector(".custom-pagination .total");
-const current = document.querySelector(".custom-pagination .current");
-const progress = document.querySelector(".custom-pagination .progress");
-const prevBtn = document.querySelector(".prev-btn");
-const nextBtn = document.querySelector(".next-btn");
-
-const totalSlides = mySwiper.slides.length;
-total.textContent = String(totalSlides).padStart(2, "0");
-
-function updatePagination() {
-  const index = mySwiper.realIndex + 1;
-
-  current.textContent = String(index).padStart(2, "0");
-
-  const percent = (index / totalSlides) * 100;
-  progress.style.width = percent + "%";
-}
-
-updatePagination();
-
-mySwiper.on("slideChangeTransitionStart", () => {
   updatePagination();
-});
 
-function resetAutoplay() {
-  mySwiper.autoplay.stop();
-  mySwiper.autoplay.start();
+  mySwiper.on("slideChangeTransitionStart", () => {
+    updatePagination();
+  });
+
+  function resetAutoplay() {
+    mySwiper.autoplay.stop();
+    mySwiper.autoplay.start();
+  }
+
+  prevBtn.addEventListener("click", () => {
+    mySwiper.slidePrev();
+    resetAutoplay();
+  });
+
+  nextBtn.addEventListener("click", () => {
+    mySwiper.slideNext();
+    resetAutoplay();
+  });
+
+  mySwiper.on("touchEnd", () => {
+    resetAutoplay();
+  });
 }
-
-prevBtn.addEventListener("click", () => {
-  mySwiper.slidePrev();
-  resetAutoplay();
-});
-
-nextBtn.addEventListener("click", () => {
-  mySwiper.slideNext();
-  resetAutoplay();
-});
-
-mySwiper.on("touchEnd", () => {
-  resetAutoplay();
-})
-};
 // headerChangeOnSection ------------------------------ //
 function headerChangeOnSection__init() {
   const header = document.querySelector("header");
   const section5 = document.querySelector(".sec-5");
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        header.classList.add("on-sec-5");
-      } else {
-        header.classList.remove("on-sec-5");
-      }
-    });
-  }, {
-    threshold: 0.3
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          header.classList.add("on-sec-5");
+        } else {
+          header.classList.remove("on-sec-5");
+        }
+      });
+    },
+    {
+      threshold: 0.3,
+    }
+  );
   observer.observe(section5);
-};
+}
 // headerChangeOnSection ------------------------------ //
 function bottomSelectboxDropUp__init() {
-  $("footer > .btn-wrap > .btn").click(function() {
+  $("footer > .btn-wrap > .btn").click(function () {
     $(this).find("> ul").toggleClass("bottomDropUp");
     $(this).find("> .title > a").toggleClass("rotateReverse");
   });
-};
+}
+// GSAP Trigger ------------------------------ //
+function scrollTrigger__init() {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".sec-2",
+      start: "top top",
+      end: "+=1000%",
+      pin: true,
+      scrub: 1,
+      invalidateOnRefresh: true,
+      anticipatePin: 1,
+    },
+  });
 
+  tl.fromTo(".t1", { opacity: 0, y: 0 }, { opacity: 1, y: 0, duration: 3 })
+
+    .to(".t1", { opacity: 0, y: 0, duration: 0 })
+    .fromTo(".t2", { opacity: 0, y: 0 }, { opacity: 1, y: 0, duration: 3 }, "<")
+
+    .to(".t2", { opacity: 0, y: 0, duration: 0 })
+    .fromTo(".t3", { opacity: 0, y: 0 }, { opacity: 1, y: 0, duration: 3 }, "<")
+
+    .to(".t3", { opacity: 0, y: 0, duration: 0 })
+    .fromTo(".t4", { opacity: 0, y: 0 }, { opacity: 1, y: 0, duration: 3 }, "<");
+}
 // Functions Operate Key ------------------------------ //
 menuboxDropdown__init();
 menuitemDropdown__init();
@@ -141,3 +170,4 @@ translateboxDropdown__init();
 swiperCustom__init();
 headerChangeOnSection__init();
 bottomSelectboxDropUp__init();
+scrollTrigger__init();
